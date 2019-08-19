a28e61f7c1e5c9a05e2b6ba1efe011508a22cddf
hanyuseong <43132234+hanyuseong@users.noreply.github.com>
2019-08-19 15:40:37 +0900

    111

---
+++ src/main/java/com/eproject/HansboardApplication.java
@@ -0,0 +1,13 @@
+package com.eproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansboardApplication.class, args);
+	}
+
+}
